b26809b13a7da1bd1cfa9da112e145e6dc07a835
sravankumbam <sravankr912@gmail.com>
2021-01-13 12:50:33 +0530

    sravansecondchange

--- Test1.java
+++ Test1.java
@@ -1,19 +1,5 @@
 public class Test1{
 public static void main(String args[]){
-<<<<<<< HEAD
-System.out.println("Hello I am test1");
-<<<<<<< HEAD
-System.out.println("Hello I am test5");
-=======
-System.out.println("Hello I am test2");
-<<<<<<< HEAD
->>>>>>> e5ed97fa509e85bbba9491a1c0cf84fa7b6f62c6
-=======
-System.out.println("I have changed this filw again");
-
->>>>>>> Change1
-=======
-system.out.println("sravanhai");
->>>>>>> 83d9eb7a5aa6105b50382b0ebc6684b45d6fc7b4
+sravan removed everything
 }
 }
