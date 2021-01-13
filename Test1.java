c3578328c6a7498f3c1e1a7b9733818edead4309
Aman Sharma <amanarunsharma98@gmail.com>
2021-01-13 17:04:32 +0530

    Studying stash

--- Test1.java
+++ Test1.java
@@ -2,5 +2,6 @@ public class Test1{
 public static void main(String args[]){
 System.out.println("new changes"};
 System.out.println("second new changes"};
+System.out.println("second new changes"};
 }
 }
