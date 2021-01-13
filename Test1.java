fdd5be16923cbf7f01b9f4e529d3f2ea7ebb3fda
Aman Sharma <amanarunsharma98@gmail.com>
2021-01-13 13:17:44 +0530

    syntax error changes

--- Test1.java
+++ Test1.java
@@ -1,6 +1,6 @@
 public class Test1{
 public static void main(String args[]){
 System.out.println("new changes"};
-ystem.out.println("second new changes"};
+System.out.println("second new changes"};
 }
 }
